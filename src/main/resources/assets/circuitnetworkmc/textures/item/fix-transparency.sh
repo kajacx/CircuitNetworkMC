440946cac5df4cad4b8ece0f9867c3a3b6ac7874
kajacx <kajacx@gmail.com>
2026-01-16 18:17:54 +0100

    Fix transparency in the build process

--- src/main/resources/assets/circuitnetworkmc/textures/item/fix-transparency.sh
+++ src/main/resources/assets/circuitnetworkmc/textures/item/fix-transparency.sh
@@ -7,8 +7,13 @@ if ! command -v magick &> /dev/null; then
   exit 0 # A fail-safe for other contributors since this will be part of the build process
 fi
 
+echo "Running the fix transparency script..."
+
 # Sets transparency to 0 or 255 based on a 50% threshold, transparent pixels look awful on item models
 for image in source/*.png; do
+  echo "Processing $image"
   image=$(basename "$image")
   magick "source/$image" -channel A -threshold 50% +channel "$image"
 done
+
+echo "Fix transparency script completed."
