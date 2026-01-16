43a9ef47b3e4a02d093f46939ee03bf6e179c08e
kajacx <kajacx@gmail.com>
2026-01-16 17:01:18 +0100

    Add the circuit wires texture and fix transparency

---
+++ src/main/resources/assets/circuitnetworkmc/textures/item/fix-transparency.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/sh
+set -e
+
+# Check if magick is installed
+if ! command -v magick &> /dev/null; then
+  echo "Warning: ImageMagick 'magick' command not found. You can ignore this if you are not editing textures."
+  exit 0 # A fail-safe for other contributors since this will be part of the build process
+fi
+
+# Sets transparency to 0 or 255 based on a 50% threshold, transparent pixels look awful on item models
+for image in source/*.png; do
+  image=$(basename "$image")
+  magick "source/$image" -channel A -threshold 50% +channel "$image"
+done
